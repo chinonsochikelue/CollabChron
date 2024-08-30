220d1066228dc535488f875dac5210a74e34282d
Chinonso Chikelue <163886675+chinonsochikelue@users.noreply.github.com>
2024-08-30 22:26:18 +0100

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -66,7 +66,7 @@ const Navbar = () => {
               />
             </a>
           </div>
-          <div className="order-1 xl:order-2 md:hidden">
+          <div className="order-1 xl:order-2 sm:hidden">
             <Logo />
           </div>
 
