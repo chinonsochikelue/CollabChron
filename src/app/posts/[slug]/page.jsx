a6db88b3ddedefaa6dc65d69be4ef04d95c92be8
Chinonso Chikelue <163886675+chinonsochikelue@users.noreply.github.com>
2025-01-03 22:10:42 +0100

    Update page.jsx

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -275,7 +275,7 @@ const SinglePage = async ({ params }) => {
         <div className="w-full flex flex-col md:flex-row gap-x-10 2xl:gap-x-28 mt-10">
           <div className="w-full md:w-2/3 flex flex-col">
           <div
-            className="prose prose-lg max-w-none text-slate-800 dark:text-white"
+            className="prose prose-lg max-w-none"
             dangerouslySetInnerHTML={{ __html: postData.desc }}
           />
             <div className="w-full px-0 md:px-10 py-8 2xl:px-20">
