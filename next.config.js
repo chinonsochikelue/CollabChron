cdc0f6ae0fec8130d16dcb2d9c8888e833f079d8
Chinonso Chikelue <163886675+chinonsochikelue@users.noreply.github.com>
2025-01-04 23:26:54 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,63 +12,6 @@ const nextConfig = {
       },
     ],
   },
-  
-  async headers() {
-    if (process.env.NODE_ENV !== 'production') {
-      return [];
-    }
-
-    return [
-      {
-        source: '/:all*(css|js|gif|svg|jpg|jpeg|png|woff|woff2)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-      {
-        source: '/:all*.html',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=60, must-revalidate',
-          },
-        ],
-      },
-      {
-        source: '/:all*(woff|woff2)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-      {
-        source: '/:all*',
-        locale: false,
-        headers: [
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains; preload',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; img-src 'self' data:; script-src 'self'",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
