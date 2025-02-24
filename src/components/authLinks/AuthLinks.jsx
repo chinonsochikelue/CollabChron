a05b60bb7a0f4dd1bb243c9a83a12b06557e18a2
chinonsochikelue <chinonsochikelue556@gmail.com>
2025-02-24 15:36:57 +0000

    modified:   src/components/authLinks/AuthLinks.jsx

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -2,43 +2,39 @@
 import Link from "next/link";
 import styles from "./authLinks.module.css";
 import { useState } from "react";
-import { signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 
-const AuthLinks = () => {
+const AuthLinks = ({ setMenu }) => {
   const [open, setOpen] = useState(false);
 
   const { status, data: session } = useSession();
-
   return (
     <>
       {status === "unauthenticated" ? (
-        <Link href="/login" className="cursor-pointer">
+        <Link href="/login" className="cursor-pointer" onClick={() => setMenu(false)}>
           Login
         </Link>
       ) : (
-        <>
-          <Link href="/write" className="cursor-pointer">
+        <div
+          className="flex items-center justify-between cursor-pointer"
+          onClick={() => setOpen(!open)}
+        >
+          <div className="flex items-center">
+
+            <Image
+              src={session?.user?.image}
+              alt="profile"
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+            <p className="ml-2">{session?.user?.name}</p>
+          </div>
+          <Link href="/write" className="cursor-pointer" onClick={() => setMenu(false)}>
             Write
           </Link>
-          {session?.user?.image ? (
-            <div className="flex flex-row text-base items-center gap-2">
-            <Image
-              src={session.user.image}
-              alt={session?.user?.name + " Profile"}
-              className="cursor-pointer w-10 h-10 rounded-full"
-              width={600}
-              height={600}
-              onClick={signOut}
-            />
-            <h2>{session?.user?.name}</h2>
-            </div>
-          ) : (
-            <span className={styles.link} onClick={signOut}>
-              Logout
-            </span>
-          )}
-        </>
+        </div>
       )}
     </>
   );
